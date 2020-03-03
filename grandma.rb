cba16150e1dd169de9678cce47ba55ac6ef56bc8
Pharia Le <pharia.le@gmail.com>
2020-03-03 04:26:46 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -15,9 +15,9 @@
 def speak_to_grandma(str)
   if str == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif str == str.downcase
+  elsif str != str.upcase
     return "HUH?! SPEAK UP, SONNY!"
-  elsif str == str.upcase
+  else
     return "NO, NOT SINCE 1938!"
   end
 end
